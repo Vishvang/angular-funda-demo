70e66eb0203e026161cb7ca55da2b1a695ac5e1c
Vishvang <vishvangp.8@gmail.com>
2018-06-09 10:50:57 -0400

    Step by step achieving basic functionality. 1- pass array of characters from tabs to list to item to display. 2- Pass ele from character array on the base of their side, initially we are sending charcters having both the side - 'dark' and 'light' means 'all' 3- Apply css active class on the base of tab selection.

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tabs.component.css']
 })
 export class TabsComponent implements OnInit {
+  allCharacter = [
+    { name: 'David', side: ''},
+    { name: 'Mac', side: ''}
+  ];
+
+  choosenList = 'all';
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onChoose(selectSide) {
+    this.choosenList = selectSide;
+  }
+
+  getCharacters() {
+    if (this.choosenList === 'all') {
+      return this.allCharacter;
+    }
+    return this.allCharacter.filter((char) => {
+      return char.side === this.choosenList;
+    });
+  }
 }
